b4e8396c04dd0c728caf44e37f86c0775918083a
Sait Ramazan <70922350+srgul@users.noreply.github.com>
2022-03-30 03:08:53 +0300

    Add project files.

---
+++ WebApplication2/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplication2.model;
+
+namespace WebApplication2.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) 
+        {
+                             
+        }
+
+        public DbSet<Category> Category { get; set; }
+
+    }
+}
